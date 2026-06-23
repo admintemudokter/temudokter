d7103bd81ec1cb5f7066cc09a314b6a873795946
Joko Hidayat <jojo@MacBook-Air-Joko.local>
2026-06-23 16:44:03 +0700

    Fix logo text from KonsulKU to TemuDokter in patient forms

--- resources/views/patient/waiting.blade.php
+++ resources/views/patient/waiting.blade.php
@@ -14,7 +14,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
                 </svg>
             </div>
-            <span class="font-heading font-bold text-white text-lg">Konsul<span class="text-teal-300">KU</span></span>
+            <span class="font-heading font-bold text-white text-lg">Temu<span class="text-teal-300">Dokter</span></span>
         </div>
 
         {{-- Animated ring --}}
