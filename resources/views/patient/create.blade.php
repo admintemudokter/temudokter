300ebccdaa73063dd8a66c0325bf96b5646e67f1
Joko Hidayat <jojo@MacBook-Air-Joko.local>
2026-06-24 14:57:47 +0700

    Update views (manual edits by user)

--- resources/views/patient/create.blade.php
+++ resources/views/patient/create.blade.php
@@ -15,7 +15,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
                 </svg>
             </div>
-            <span class="font-heading font-bold text-white">Temu<span class="text-teal-300">Dokter</span></span>
+            <span class="font-heading font-bold text-xl text-white">Temu <span class="text-emerald-400">Dokter</span></span>
         </a>
         <div class="text-white/60 text-sm">Formulir Konsultasi</div>
     </div>
