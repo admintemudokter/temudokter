c123c528d8f45b026ea14b38e26ab57d12c28575
Joko Hidayat <jojo@MacBook-Air-Joko.local>
2026-06-27 01:57:04 +0700

    fix: remove maxlength and change rt_rw to text to allow unlimited address length

--- resources/views/patient/create.blade.php
+++ resources/views/patient/create.blade.php
@@ -200,7 +200,7 @@
                                 {{-- Alamat Lengkap --}}
                                 <div>
                                     <label class="form-label" for="rt_rw">Alamat Lengkap <span class="text-rose-500">*</span></label>
-                                    <input type="text" id="rt_rw" name="rt_rw" maxlength="500" class="form-input" placeholder="Contoh: Jl. Merdeka No. 10, RT 01/RW 02" x-model="form.rt_rw" required>
+                                    <input type="text" id="rt_rw" name="rt_rw" class="form-input" placeholder="Contoh: Jl. Merdeka No. 10, RT 01/RW 02" x-model="form.rt_rw" required>
                                 </div>
                             </div>
                         </div>
